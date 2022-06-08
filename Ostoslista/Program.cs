96028e9e2534255b5a24a3dbcb3a7536049abbbc
anders von wendt <andersvonwendt@gmail.com>
2022-06-08 10:39:18 +0300

    first

---
+++ Ostoslista/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Ostoslista
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Syötä tuote");
+            string tuote = Console.ReadLine();
+
+            string path = @"C:\Users\Anders\ostoslista.txt";
+
+            File.WriteAllText(path, tuote);
+        }
+    }
+}
+
